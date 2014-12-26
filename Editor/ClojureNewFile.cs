a13a93dc298d214278e92befcb5ee3cc97ef0b79
pls <pls@algoterranean.com>
2014-12-26 09:55:47 -0600

    Creating new components now uses paths from configure.edn. Added Editor path to configure.edn.

--- Editor/ClojureNewFile.cs
+++ Editor/ClojureNewFile.cs
@@ -8,30 +8,53 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.ProjectWindowCallback;
 using clojure.lang;
+using clojure.clr.api;
+
+
+
 
 public class ClojureNewFile : EditorWindow {
   // TODO support nested folders, hyphen/underscores
-  // TODO read file paths from config
   [MenuItem ("Assets/Create/Clojure Component", false, 90)]
   [MenuItem ("Arcadia/New Component", false, 90)]
   public static void NewComponent () {
+	var paths = GetPaths();
     var DoCreateScriptAsset = Type.GetType("UnityEditor.ProjectWindowCallback.DoCreateScriptAsset, UnityEditor");
     
     ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0,
       ScriptableObject.CreateInstance(DoCreateScriptAsset) as UnityEditor.ProjectWindowCallback.EndNameEditAction,
-      "Assets/Clojure/Scripts/new-component.clj",
+      paths[0] + "/new-component.clj",
       null,
-      "Assets/Clojure/Editor/new-component-template.clj.txt");
+      paths[2] + "/new-component-template.clj.txt");
   }
   
   [MenuItem ("Arcadia/New File", false, 91)]
   public static void NewFile () {
+	var paths = GetPaths();
     var DoCreateScriptAsset = Type.GetType("UnityEditor.ProjectWindowCallback.DoCreateScriptAsset, UnityEditor");
     
     ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0,
       ScriptableObject.CreateInstance(DoCreateScriptAsset) as UnityEditor.ProjectWindowCallback.EndNameEditAction,
-      "Assets/Clojure/Scripts/new-file.clj",
+      paths[0] + "/new-file.clj",
       null,
-      "Assets/Clojure/Editor/new-file-template.clj.txt");
+      paths[2] + "/new-file-template.clj.txt");
   }
+
+
+
+  private static String[] GetPaths() {
+	RT.load("arcadia/config");
+	// load config file and get the paths from it
+	RT.var("arcadia.config", "update-from-default-location!").invoke();
+	var val_in = RT.var("arcadia.config", "value-in");
+	PersistentVector paths = (PersistentVector) val_in.invoke(Clojure.read("[:compiler :load-path]"));
+	// TODO change config file to use a map so that the order doesn't matter here
+	String scripts_path = (String) paths.nth(0);
+	String lib_path = (String) paths.nth(1);
+	String editor_path = (String) paths.nth(2);
+	
+	return new String[]{scripts_path, lib_path, editor_path};
+  }
+	
+
 }
